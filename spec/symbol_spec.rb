ed8f14ef58975a3171920bb60291d2a887064ff6
Ale Muñoz <bomberstudios@gmail.com>
2008-12-25 09:27:07 +0100

    Whitespace cleanup

--- spec/symbol_spec.rb
+++ spec/symbol_spec.rb
@@ -5,11 +5,11 @@ describe Symbol do
     get_resources
     Stone.start(STONE_ROOT/"sandbox_for_specs", @resources)
   end
-  
+
   it "should return a correct Query when a comparison method is used" do
     :name.gt.should be_instance_of(Stone::Query)
     :name.gt.op.should == ".>"
-    
+
     :name.matches.should be_instance_of(Stone::Query)
     :name.matches.op.should == ".=~"
   end
