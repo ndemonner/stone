a372f4d91d0ea92871e4b4b65eab9e17bfbf023b
Ale Muñoz <bomberstudios@gmail.com>
2009-01-13 11:45:16 +0100

    Whoops...

--- lib/stone/resource.rb
+++ lib/stone/resource.rb
@@ -32,7 +32,9 @@ module Stone
           base.class_eval <<-EOS, __FILE__, __LINE__
             def initialize(hash = {})
               self.id = self.next_id_for_klass(self.class)
-              update_attributes(hash)
+              unless hash.blank?
+                update_attributes(hash)
+              end
               @@fields[self.model].each do |f|
                 instance_variable_set("@"+f[:name].to_s,[]) if f[:klass] == Array
               end
